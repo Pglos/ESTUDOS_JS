05bb842d22eaea8e82bb56c197c4f5dc3530fdf7
Pedro Gabriel <102000517+Pglos@users.noreply.github.com>
2025-08-25 18:04:03 -0300

    Create estudos.js

---
+++ Javascript-logica/estudos.js
@@ -0,0 +1,14 @@
+/* AULA 1 
+
+OPERADORES DE COMPARAÇÃO 
+
+-> ( > )   - MAIOR QUE
+-> ( >= )  - MAIOR QUE OU IGUAL A 
+-> ( < )   - MENOR QUE
+-> ( <= )  - MAIOR QUE OU IGUAL 
+-> ( == )  - IGUALDADE (VALOR)
+-> ( === ) - IGUALDADE ESTRITA (VALOR E TIPO) 
+-> ( != )  - DIFERENTE (VALOR)
+-> ( !== ) - DIFRENTE ESTRITO (VALOR E TIPO)
+*/
+
